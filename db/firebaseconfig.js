09f932263cb0658cb13a41a9c5a58a1270f948f3
Mauricio rubio <rubiom831@gmail.com>
2024-10-02 21:40:16 -0600

    ayuda para solucionar el error porfavor

---
+++ db/firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCH2aEBM7vQ9UizJS1aFsiRLP4Qu6V7vrI",
+  authDomain: "estadisticas-c64d2.firebaseapp.com",
+  projectId: "estadisticas-c64d2",
+  storageBucket: "estadisticas-c64d2.appspot.com",
+  messagingSenderId: "309018705623",
+  appId: "1:309018705623:web:96f57880ac88077c0d3a92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
